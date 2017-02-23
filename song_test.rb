1b17e4d2548a80088f6a8a2f540259cb2b1df50d
Nick Wu <nickwu78@gmail.com>
2017-02-23 14:53:25 +1100

    Fixed unit tests

--- song_test.rb
+++ song_test.rb
@@ -4,8 +4,8 @@ require_relative 'song'
 class SongTest < Test::Unit::TestCase
 
   def test_user_input_country_should_be_valid
-  song = Song.new.recommend('US','romance')
-  actual = get_country
+  song = Song.new
+  actual = song.recommend('US','romance')
   expected = "US"
   message = "Country code should be valid"
   assert_equal(expected, actual, message)
